8463838eb703ef85fc5c235b7e8c84a7f2d60d5d
GuNyangE <noel7145@gmail.com>
2025-06-07 02:02:31 +0900

    fix: 백엔드 url 환경변수 불러오기 수정

--- src/app/api/(env)/rest-api-url/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  const url = process.env.REST_API_URL;
-
-  if (!url) {
-    return new Response('Missing REST_API_URL', { status: 500 });
-  }
-  return Response.json({ url });
-}
