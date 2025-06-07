78fcc1c3089e81dd72d297625985424771956cb2
kangsuyeong <ekdh16@naver.com>
2025-06-07 19:53:13 +0900

    feat: 아이콘 추가

---
+++ src/shared/ui/icons/24/ChevronRight24.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ChevronRight24 = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+      <path
+        fill="#222323"
+        fillRule="evenodd"
+        d="M16.5 12a.5.5 0 0 1-.146.354l-5 5a.5.5 0 0 1-.708-.708L15.293 12l-4.647-4.646a.5.5 0 0 1 .708-.708l5 5A.5.5 0 0 1 16.5 12"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+};
+
+export default ChevronRight24;
---
+++ src/widgets/mypage/icons/index.ts
@@ -0,0 +1,5 @@
+import BookmarkIcon from '@/widgets/mypage/icons/BookmarkIcon';
+import CopyIcon from '@/widgets/mypage/icons/CopyIcon';
+import DeleteAccountIcon from '@/widgets/mypage/icons/DeleteAccountIcon';
+
+export { BookmarkIcon, DeleteAccountIcon, CopyIcon };
