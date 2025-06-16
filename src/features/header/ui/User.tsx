9f3c155ef8f0a53419e45ca5bac7b869920cd3ff
GuNyangE <noel7145@gmail.com>
2025-06-16 21:43:31 +0900

    feature: zustand persist 추가

--- src/features/header/ui/User.tsx
+++ src/features/header/ui/User.tsx
@@ -3,16 +3,16 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Hamburger24, UserProfile28, UserProfile32 } from '@/shared/ui/';
 import { LoginModal } from '@/features/login';
-import { LocalStorage } from '@/shared/model';
 import { getUserInfo } from '../api';
 import { useQuery } from '@tanstack/react-query';
 import { UserProfile } from '../types';
 import { useRouter } from 'next/navigation';
+import { useUserStore } from '@/shared/store';
 
 export default function User() {
   const router = useRouter();
 
-  const jwt = LocalStorage.getItem('accessToken');
+  const { jwt, logout } = useUserStore();
 
   const [dropMenuOpen, setDropMenuOpen] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -105,7 +105,7 @@ export default function User() {
                 const confirmSignout = confirm('정말 로그아웃 하실 건가요?');
 
                 if (confirmSignout) {
-                  LocalStorage.removeItem('accessToken');
+                  logout();
                   router.refresh();
                 }
               }}
---
+++ src/shared/store/userStore.ts
@@ -0,0 +1,24 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+interface UserState {
+  jwt: string | null;
+  memberId: number | null;
+  login: (jwt: string, memberId: number) => void;
+  logout: () => void;
+}
+
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      jwt: null,
+      memberId: null,
+      login: (jwt, memberId) => set({ jwt, memberId: memberId }),
+      logout: () => set({ jwt: null, memberId: null }),
+    }),
+    {
+      name: 'userInfo',
+      partialize: (state) => ({ jwt: state.jwt, nickname: state.memberId }),
+    }
+  )
+);
