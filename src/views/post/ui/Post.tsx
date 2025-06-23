8cfd726ab527c35400e91fa960542f2cb74c5b43
kangsuyeong <ekdh16@naver.com>
2025-06-23 17:08:48 +0900

    feat: 게시물 상세 구현

--- src/views/post/ui/Post.tsx
+++ src/views/post/ui/Post.tsx
@@ -1,18 +1,29 @@
+import { axiosPost } from '@/shared/api';
+import { PostDetailResponse } from '@/shared/types';
 import { Comments } from '@/widgets/comments';
 import { Content, Header } from '@/widgets/post';
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 interface PostProps {
-  /**@param {string} postId 게시글 아이디(path parameter이므로 string) */
-  postId: string;
+  /**@param {number} postId 게시글 아이디 */
+  postId: number;
+  /**@param {number} postId 게시판 아이디 */
+  boardId: number;
 }
 
-export default function Post({}: PostProps) {
+export default function Post({ postId, boardId }: PostProps) {
+  const { data: post } = useQuery({
+    queryKey: ['postDetail', boardId, postId],
+    queryFn: () => axiosPost<PostDetailResponse>(boardId, postId),
+    select: (data) => data.postInfoDto,
+  });
+  if (!post) return null;
+
   return (
     <div className="flex border-r border-gray-200 bg-white pr-8">
       <div className="flex w-full flex-col gap-y-8">
-        <Header />
-        <Content />
+        <Header post={post} />
+        <Content post={post} />
         <Comments />
       </div>
     </div>
