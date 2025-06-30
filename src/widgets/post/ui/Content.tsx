0cc88f535c1da005513e0a8f8d46cd45eaef920b
kangsuyeong <ekdh16@naver.com>
2025-06-30 14:36:47 +0900

    feat: delta -> HTML, XSS 방지 로직 추가

--- src/widgets/post/ui/Content.tsx
+++ src/widgets/post/ui/Content.tsx
@@ -1,7 +1,21 @@
 'use client';
 
 import { PostDetail } from '@/shared/types';
+import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+import DOMPurify from 'dompurify';
 
 export default function Content({ post }: { post: PostDetail }) {
-  return <div dangerouslySetInnerHTML={{ __html: post.content }} />;
+  // 임시 로직  Delta → HTML
+  let html = '';
+  try {
+    const delta = JSON.parse(post.content);
+    const converter = new QuillDeltaToHtmlConverter(delta.ops, {});
+    html = converter.convert();
+  } catch {
+    html = post.content;
+  }
+
+  // XSS 방지 처리
+  html = DOMPurify.sanitize(html);
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
 }
