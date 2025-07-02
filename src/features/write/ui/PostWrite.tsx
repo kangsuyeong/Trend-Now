410b1007e93a47c5b968308ccb4edd3304b2a73a
kangsuyeong <ekdh16@naver.com>
2025-07-02 22:11:09 +0900

    feat: submit시 Delta에서 img-id 추출 로직 추가

--- src/features/write/ui/PostWrite.tsx
+++ src/features/write/ui/PostWrite.tsx
@@ -5,6 +5,7 @@ import { useUserStore } from '@/shared/store';
 import { RichTextEditorHandle } from '@/shared/types';
 import { useRouter } from 'next/navigation';
 import { useRef } from 'react';
+import type { Delta } from 'quill';
 
 interface PostWriteProps {
   /** 게시판 이름 */
@@ -20,14 +21,27 @@ const PostWrite = ({ boardName, boardId, path }: PostWriteProps) => {
   const { jwt } = useUserStore();
   const editorRef = useRef<RichTextEditorHandle>(null); // 에디터 내용(DOM)이나 메서드에 접근하기 위한 ref
   const titleRef = useRef<HTMLInputElement>(null); // 제목 저장하는 ref
-  const imageIdsRef = useRef<number[]>([]); // 이미지 index 저장하는 ref
+
+  // Delta에서 img id 추출함수
+  const extractImageIdsFromDelta = (delta: Delta): number[] => {
+    const ids: number[] = [];
+    delta.ops.forEach((op) => {
+      if (typeof op.insert === 'object' && op.insert !== null) {
+        const insert = op.insert as { customImage?: { id: string | number } };
+        if (insert.customImage?.id) {
+          ids.push(Number(insert.customImage.id));
+        }
+      }
+    });
+
+    return ids;
+  };
 
   const handleSubmit = async () => {
     const title = titleRef.current?.value;
     const delta = editorRef.current?.getContents();
     const content = JSON.stringify(delta);
-    const imageIds = imageIdsRef.current;
-
+    const imageIds = extractImageIdsFromDelta(delta!);
     if (!title || !content) return alert('제목 또는 내용을 입력해주세요');
 
     if (!jwt) {
@@ -42,7 +56,6 @@ const PostWrite = ({ boardName, boardId, path }: PostWriteProps) => {
       boardName={boardName}
       titleRef={titleRef}
       editorRef={editorRef}
-      imageIdsRef={imageIdsRef}
       onSubmit={handleSubmit}
     />
   );
