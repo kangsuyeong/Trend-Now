7f7a737719095702b7eb72879068e7ec9ce65eba
kangsuyeong <ekdh16@naver.com>
2025-07-07 08:29:36 +0900

    refactor: dropdownmenu ui 컴포넌트 분리

--- src/features/post/ui/PostKebabButton.tsx
+++ src/features/post/ui/PostKebabButton.tsx
@@ -1,64 +1,32 @@
-'use client';
-
-import React, { useEffect, useRef, useState } from 'react';
 import { Delete, Write } from './icons';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { Dropdownmenu, Kebab32 } from '@/shared/ui';
 
 export default function PostKebabButton() {
-  const pathname = usePathname();
-  const [dropMenuOpen, setDropMenuOpen] = useState<boolean>(false);
-
-  const dropMenuButtonRep = useRef<HTMLSpanElement>(null);
-  const dropMenuRep = useRef<HTMLSpanElement>(null);
-
-  const handleDropMenuToggle = () => {
-    setDropMenuOpen((prev) => {
-      return !prev;
-    });
-  };
-
-  useEffect(() => {
-    const handleDropMenuClose = (e: MouseEvent) => {
-      if (
-        dropMenuOpen &&
-        !dropMenuButtonRep.current?.contains(e.target as Node) &&
-        !dropMenuRep.current?.contains(e.target as Node)
-      ) {
-        setDropMenuOpen(false);
-      }
-    };
-
-    document.addEventListener('click', handleDropMenuClose);
-    return () => {
-      document.removeEventListener('click', handleDropMenuClose);
-    };
-  }, [dropMenuOpen]);
-
   return (
-    <span className="relative select-none">
-      <span
-        ref={dropMenuButtonRep}
-        onClick={handleDropMenuToggle}
-        className="flex h-10 w-10 cursor-pointer appearance-none items-center justify-center rounded-lg border border-gray-200 before:h-8 before:w-8 before:content-[url('/images/icons/icon_kebab_32x32.svg')]"
-      />
-      {dropMenuOpen && (
-        <span
-          ref={dropMenuRep}
-          className="absolute right-0 z-10 mt-2 flex h-fit w-[12.5rem] flex-col gap-y-1 rounded-[1.25rem] bg-white p-4 shadow-[0px_2px_10px_0px_rgba(0,_0,_0,_0.08)]"
-        >
-          <Link
-            href={`${pathname}/edit`}
-            className="flex h-11 w-full cursor-pointer items-center gap-x-1.5 rounded-xl p-2 text-md font-medium text-gray-800 hover:bg-gray-100"
-          >
-            <Write />
-            <span>게시글 수정</span>
-          </Link>
-          <span className="flex h-11 w-full cursor-pointer items-center gap-x-1.5 rounded-xl p-2 text-md font-medium text-negative hover:bg-gray-100">
-            <Delete /> <span>게시글 삭제</span>
-          </span>
+    <Dropdownmenu
+      trigger={
+        <span className="flex h-10 w-10 items-center justify-center rounded-lg border border-gray-200">
+          <Kebab32 className="text-gray-500" />
         </span>
-      )}
-    </span>
+      }
+      items={[
+        {
+          content: (
+            <>
+              <Write />
+              <span>게시글 수정</span>
+            </>
+          ),
+        },
+        {
+          content: (
+            <>
+              <Delete />
+              <span className="text-negative">게시글 삭제</span>
+            </>
+          ),
+        },
+      ]}
+    />
   );
 }
