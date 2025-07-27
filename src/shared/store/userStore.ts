e6df96b8a2ffeb5c03c2012b76af38b5bf66ecd1
kangsuyeong <ekdh16@naver.com>
2025-07-27 17:42:15 +0900

    chore: 테스트 코드 삭제

--- src/shared/store/userStore.ts
+++ src/shared/store/userStore.ts
@@ -11,7 +11,7 @@ interface UserState {
 export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
-      accessToken: '',
+      accessToken: null,
       memberId: null,
       login: (accessToken, memberId) => set({ accessToken, memberId }),
       logout: () => set({ accessToken: null, memberId: null }),
