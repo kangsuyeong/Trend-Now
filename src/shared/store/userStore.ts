5dc2c1d00e15ab9e6ef6ffa28c71e9b6686d7f90
GuNyangE <noel7145@gmail.com>
2025-07-29 17:15:11 +0900

    feature: userStore isAuthenticated 변수 추가

--- src/shared/store/userStore.ts
+++ src/shared/store/userStore.ts
@@ -3,6 +3,7 @@ import { persist } from 'zustand/middleware';
 
 interface UserState {
   accessToken: string | null;
+  isAuthenticated: boolean;
   login: (accessToken: string) => void;
   logout: () => void;
 }
@@ -11,12 +12,16 @@ export const useUserStore = create<UserState>()(
   persist(
     (set) => ({
       accessToken: null,
-      login: (accessToken) => set({ accessToken }),
-      logout: () => set({ accessToken: null }),
+      isAuthenticated: false,
+      login: (accessToken) => set({ accessToken, isAuthenticated: true }),
+      logout: () => set({ accessToken: null, isAuthenticated: false }),
     }),
     {
       name: 'userInfo',
-      partialize: (state) => ({ accessToken: state.accessToken }),
+      partialize: (state) => ({
+        accessToken: state.accessToken,
+        isAuthenticated: state.isAuthenticated,
+      }),
     }
   )
 );
