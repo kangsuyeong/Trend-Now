fbc6f1c6098893a902002aea8b67deafae6e9f7e
kangsuyeong <ekdh16@naver.com>
2025-08-14 09:50:46 +0900

    fix: stale time 제거

--- src/providers/queryClient.ts
+++ src/providers/queryClient.ts
@@ -4,7 +4,6 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
         refetchOnWindowFocus: false,
       },
     },
