89ef22a31f2ca64bd58b7630f4488539bea94f36
GuNyangE <noel7145@gmail.com>
2025-09-07 02:21:01 +0900

    fix: 푸터 내용 수정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -33,6 +33,7 @@ import UserProfile20 from './icons/20/UserProfile20';
 import UserProfile64 from './icons/64/UserProfile64';
 import ChevronRight24 from './icons/24/ChevronRight24';
 import Trendnow from './icons/Trendnow';
+import TrendnowWhite from './icons/32/TrendnowWhite';
 
 /** Text Fields */
 import { Dropdown, DropdownItem } from './text-field/Dropdown';
@@ -83,6 +84,7 @@ export {
   PageRightChevron,
   PageRightDoubleChevron,
   Trendnow,
+  TrendnowWhite,
   UserProfile64,
   Settings20,
   ChevronRight24,
