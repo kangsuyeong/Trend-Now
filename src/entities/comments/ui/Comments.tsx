38a6274a8be17084d2230fa264bedc369bf0d985
kangsuyeong <ekdh16@naver.com>
2025-09-08 19:56:29 +0900

    fix: 게시물 상세 페이지 디자인 수정

--- src/entities/comments/ui/Comments.tsx
+++ src/entities/comments/ui/Comments.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import Comment from './Comment';
 import { useQuery } from '@tanstack/react-query';
 import { useUserStore } from '@/shared/store';
@@ -26,33 +26,38 @@ export default function Comments({ postId, boardId }: CommentsProps) {
   });
 
   return (
-    <div className="flex flex-col gap-y-7 border-t border-gray-200 pt-8">
-      <div className="flex flex-col gap-y-7 rounded-3xl bg-gray-100 p-6">
+    <div className="flex flex-col border-t border-gray-200 pt-4">
+      <div className="flex flex-col gap-y-5 rounded-3xl bg-gray-100 p-6">
         <span className="flex items-center gap-x-2.5 before:inline-block before:h-5 before:w-[3px] before:rounded-full before:bg-brand-500">
           <span className="flex items-center gap-x-2">
             <span className="text-xl font-bold text-gray-800">전체 댓글</span>
             <span className="text-xl font-bold text-brand-500">{data?.length}</span>
           </span>
         </span>
-        <div className="flex flex-col gap-y-2">
-          <div className="flex flex-col divide-y divide-gray-200">
+
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col">
             {data &&
               data.map((item, idx) => (
-                <Comment
-                  key={idx}
-                  userName={item.writer}
-                  date={item.createdAt}
-                  content={item.content}
-                  showMenu={item.myComments}
-                  boardId={boardId}
-                  postId={postId}
-                  commentId={item.commentId}
-                  refetch={refetch}
-                />
+                <Fragment key={idx}>
+                  <Comment
+                    key={idx}
+                    userName={item.writer}
+                    date={item.createdAt}
+                    content={item.content}
+                    showMenu={item.myComments}
+                    boardId={boardId}
+                    postId={postId}
+                    commentId={item.commentId}
+                    refetch={refetch}
+                  />
+                  <hr className="my-5 border-gray-200" />
+                </Fragment>
               ))}
           </div>
+
+          {isAuthenticated && <WriteComment boardId={boardId} postId={postId} refetch={refetch} />}
         </div>
-        {isAuthenticated && <WriteComment boardId={boardId} postId={postId} refetch={refetch} />}
       </div>
     </div>
   );
