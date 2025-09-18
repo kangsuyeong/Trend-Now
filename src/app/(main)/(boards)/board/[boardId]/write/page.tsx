84c4c01a72d1d2769a04ee071ef7b87b6bf34a36
kangsuyeong <ekdh16@naver.com>
2025-09-18 19:56:14 +0900

    chore: LoginGuard 컴포넌트 주석처리

--- src/app/(main)/(boards)/board/[boardId]/write/page.tsx
+++ src/app/(main)/(boards)/board/[boardId]/write/page.tsx
@@ -1,6 +1,6 @@
 import { BoardName } from '@/entities/board';
 import { PostWrite } from '@/features/write';
-import { LoginGuard } from '@/shared/middleware';
+// import { LoginGuard } from '@/shared/middleware';
 import Image from 'next/image';
 
 export default async function Page({ params }: { params: Promise<{ boardId: string }> }) {
@@ -8,22 +8,22 @@ export default async function Page({ params }: { params: Promise<{ boardId: stri
   const path = `/board/${boardId}`;
 
   return (
-    <LoginGuard redirectPath={path}>
-      <div className="flex flex-col gap-4">
-        <BoardName
-          boardId={Number(boardId)}
-          className="border-b border-gray-200 pb-4 text-2xl font-bold"
-          icon={
-            <Image
-              src="/images/icons/icon_penceil_32X32.png"
-              width={32}
-              height={32}
-              alt="연필 아이콘"
-            />
-          }
-        />
-        <PostWrite boardId={Number(boardId)} path={path} />
-      </div>
-    </LoginGuard>
+    // <LoginGuard redirectPath={path}>
+    <div className="flex flex-col gap-4">
+      <BoardName
+        boardId={Number(boardId)}
+        className="border-b border-gray-200 pb-4 text-2xl font-bold"
+        icon={
+          <Image
+            src="/images/icons/icon_penceil_32X32.png"
+            width={32}
+            height={32}
+            alt="연필 아이콘"
+          />
+        }
+      />
+      <PostWrite boardId={Number(boardId)} path={path} />
+    </div>
+    // </LoginGuard>
   );
 }
